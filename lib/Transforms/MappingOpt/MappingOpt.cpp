a053f9422ab38bd5d1a9d4a6e3785e65fbe1f5ec
Youngbin Kim <yb.kim@yonsei.ac.kr>
2017-07-10 16:32:52 +0900

    Fix #1 by checking function linkage type is internal

--- lib/Transforms/MappingOpt/MappingOpt.cpp
+++ lib/Transforms/MappingOpt/MappingOpt.cpp
@@ -190,6 +190,17 @@ namespace {
             unsigned long cost = 0, maxConflict = 0;
             std::pair <Function *, Function *> maxConflictingFunctions;
 
+            for(auto i = costInfo.begin(); i != costInfo.end(); ) {
+                std::pair <Function *, Function *> key = i->first;
+                Function *f1 = key.first, *f2 = key.second;
+                if(f1->hasInternalLinkage() || f2->hasInternalLinkage()) {
+                    errs() << f1->getName() << "\t" << f2->getName() << "\n";
+                    costInfo.erase(i++);
+                } else {
+                    i++;
+                }
+            }
+
             for(auto i = costInfo.begin(); i != costInfo.end(); i++) {
                 std::pair <Function *, Function *> key = i->first;
                 unsigned long conflict = i->second;
